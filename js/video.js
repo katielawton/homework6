4fcd7c799e8d07cc0514591ed2bca0426e7f986f
katielawton <45472321+katielawton@users.noreply.github.com>
2020-11-15 11:53:35 -0500

    fixed java

--- js/video.js
+++ js/video.js
@@ -1,40 +1,50 @@
 var video;
 
+// video function
 window.addEventListener("load", function() {
 	video = document.querySelector("#myVideo");
 });
 
+// play function
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+
+	// initial volumn level when video begins
 	document.querySelector("#volume").innerHTML = "100%"
 });
 
+// pause function
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video")
 	video.pause()
 });
 
+// slow down function
 document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate = video.playbackRate * 0.9;
 	console.log("Video Speed: " + video.playbackRate);
 });
 
+// speed up function
 document.querySelector("#faster").addEventListener("click", function() {
 	video.playbackRate = video.playbackRate * 1.1;
 	console.log("Video Speed: " + video.playbackRate);
 });
 
+// skip five seconds function
 document.querySelector("#skip").addEventListener("click", function() {
 	video.currentTime = video.currentTime + 5
+	// when video hits end
 	if (video.ended) {
 		video.load();
 		video.play();
-		playbackRate =1;
+		video.playbackRate =1;
 	}
 	console.log("Current location is " + video.currentTime)
 });
 
+// mute function
 document.querySelector("#mute").addEventListener("click", function() {
 	if (video.muted) {
 		video.muted = false;
@@ -48,6 +58,7 @@ document.querySelector("#mute").addEventListener("click", function() {
 	}
 });
 
+// volume slider
 document.querySelector("#volumeSlider").addEventListener("click", function() {
 	console.log(volumeSlider.value);
 	// volume of video
@@ -57,11 +68,13 @@ document.querySelector("#volumeSlider").addEventListener("click", function() {
 	document.querySelector("#volume").innerHTML = slider_volume + "%"
 });
 
+// black and white
 document.querySelector("#old").addEventListener("click", function() {
 	video.classList.add("oldTime");
 	console.log("Grayscale")
 });
 
+// color
 document.querySelector("#original").addEventListener("click", function() {
 	video.classList.remove("oldTime");
 	console.log("Normal color")
